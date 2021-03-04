90779e58fd92fc8de01d8e2a7151e67c0bd7a454
Patrick <admin@MacBook-Pro.home>
2021-03-04 03:17:10 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+document.body.remove("main");
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.textContent = "Patrick is the champion";
